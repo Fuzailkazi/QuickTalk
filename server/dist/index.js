be3cd06ff9f0a69bffbe4e0c1030b07ca5f8643b
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2024-12-18 21:04:41 +0530

    asdf

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,15 +5,29 @@ const wss = new ws_1.WebSocketServer({ port: 8080 });
 let userCount = 0;
 let allSockets = [];
 wss.on('connection', (socket) => {
-    allSockets.push(socket);
     console.log('user connected');
     userCount = userCount + 1;
     console.log(userCount);
     socket.on('message', (message) => {
-        console.log('got msg ' + message.toString());
-        for (let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            s.send(message.toString() + ': sent from server');
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == 'join') {
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId,
+            });
+        }
+        if (parsedMessage.type == 'chat') {
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     });
 });
