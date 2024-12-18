aecb4f5a56db3d9596d524fb9c957fadf928f22e
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2024-12-18 16:15:32 +0530

    broadcast server done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log('hello');
+import { WebSocketServer, WebSocket } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+let allSockets: WebSocket[] = [];
+
+wss.on('connection', (socket) => {
+  allSockets.push(socket);
+
+  console.log('user connected');
+  userCount = userCount + 1;
+  console.log(userCount);
+
+  socket.on('message', (message) => {
+    console.log('got msg ' + message.toString());
+    for (let i = 0; i < allSockets.length; i++) {
+      const s = allSockets[i];
+      s.send(message.toString() + ': sent from server');
+    }
+  });
+});
