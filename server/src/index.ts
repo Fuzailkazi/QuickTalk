c7f49134e855e385d2fb06273bacf84c99f9a1d0
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2024-12-18 20:04:15 +0530

    basic websocket functions done

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,21 +2,41 @@ import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+
 let userCount = 0;
-let allSockets: WebSocket[] = [];
+let allSockets: User[] = [];
 
 wss.on('connection', (socket) => {
-  allSockets.push(socket);
-
   console.log('user connected');
   userCount = userCount + 1;
   console.log(userCount);
 
   socket.on('message', (message) => {
-    console.log('got msg ' + message.toString());
-    for (let i = 0; i < allSockets.length; i++) {
-      const s = allSockets[i];
-      s.send(message.toString() + ': sent from server');
+    const parsedMessage = JSON.parse(message as unknown as string);
+    if (parsedMessage.type === 'join') {
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomId,
+      });
+    }
+
+    if (parsedMessage.type === 'chat') {
+      let currentUserRoom = null;
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i].socket == socket) {
+          currentUserRoom = allSockets[i].room;
+        }
+      }
+
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i].room == currentUserRoom) {
+          allSockets[i].socket.send(parsedMessage.payload.message);
+        }
+      }
     }
   });
 });
